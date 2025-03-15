8e6caa3b3aa93c092108ced5ccc19f24dc1a8e5a
Ivan Pechyonkin <vanchenkin@yandex.ru>
2025-03-15 15:27:37 +0300

    Update package dependencies to latest versions for improved stability and performance

---
+++ .ncurc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    reject: [
+       'react',
+       'react-dom',
+       'react-router-dom',
+       'react-router',
+       'react-router-dom',
+       'react-router',
+       '@types/react',
+       '@types/react-dom',
+       '@types/react-router-dom',
+       '@types/react-router',
+       '@types/react-router-dom',
+       '@types/react-router',
+       "eslint",
+       "@react-native-async-storage/async-storage"
+    ],
+};
